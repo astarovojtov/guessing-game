7078d701f724403a690bb8a9f434ccbf0c8ac770
astarovojtov <andrei.starovojtov@gmail.com>
2017-09-29 14:15:11 +0300

    Submit
    Submit task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.start = 0;
+      this.stop = 0;
+      this.middle = 0;
+    }
 
     setRange(min, max) {
-
+      this.start = min;
+      this.stop = max;
     }
 
     guess() {
-
+      return this.middle = Math.ceil(this.start / 2 + this.stop / 2);
     }
 
-    lower() {
+    lower() { this.stop = this.middle; }
 
-    }
-
-    greater() {
-
-    }
+    greater() { this.start = this.middle; }
 }
 
 module.exports = GuessingGame;
